84a25f5a901126fff80808d84af7d1b6b2a6e366
h3lloworld <astroneer976@gmail.com>
2023-03-12 20:56:39 +1100

    Add WAF acl and rate limiter

---
+++ modules/nginx/waf.tf
@@ -0,0 +1,48 @@
+# Create a WAF web acl
+resource "aws_wafv2_web_acl" "waf_nginx_acl" {
+  name        = "waf-web-acl"
+  description = "The acl rule for nginx server"
+  scope       = "REGIONAL"
+
+  default_action {
+    block {}
+  }
+
+  rule {
+    name     = "rate-based-rule"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 100  # limit per 5 mins, 100 minimum
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["AU"]
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "nginx-rule-metric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  tags = {
+    Name = "waf-web-acl"
+  }
+}
+
+# Associate WAF with ALB
+resource "aws_wafv2_web_acl_association" "waf_alb_association" {
+  resource_arn = aws_lb.alb_nginx.arn
+  web_acl_arn  = aws_wafv2_web_acl.waf_nginx_acl.arn
+}
