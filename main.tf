f209401bbe1bb3f6bb3d2c3764d7bec6859e00d5
h3lloworld <astroneer976@gmail.com>
2023-03-12 23:19:03 +1100

    Update false reference

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "ecs_nginx" {
   source = "./modules/nginx"
 
   vpc_id = data.aws_vpc.default.id
-  subnet_ids = data.aws_subnet_ids.ids
+  subnet_ids = data.aws_subnet_ids.default.ids
   ecs_sg_id = aws_security_group.nginx_http_sg.id
   alb_sg_id = aws_security_group.alb_http_sg.id
   ecs_role_arn = aws_iam_role.ecs_role.arn
