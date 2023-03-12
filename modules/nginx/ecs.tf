8ac67a771d06b6a412ed73f5ae71ae7ecd5d50cb
h3lloworld <astroneer976@gmail.com>
2023-03-12 21:32:41 +1100

    Add IAM role/policy for ECS

--- modules/nginx/ecs.tf
+++ modules/nginx/ecs.tf
@@ -15,7 +15,7 @@ module "ecs" {
 
   tags = {
     Environment = "Development"
-    Name = "nginx-cluster"
+    Name        = "nginx-cluster"
   }
 }
 
@@ -26,20 +26,20 @@ resource "aws_ecs_task_definition" "nginx_task" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  container_definitions    = jsonencode([
+  container_definitions = jsonencode([
     {
-    name = "nginx",
-    image = "nginx:latest",
-    cpu = 256,
-    memory = 512,
-    essential = true,
-    portMappings = [
+      name      = "nginx",
+      image     = "nginx:latest",
+      cpu       = 256,
+      memory    = 512,
+      essential = true,
+      portMappings = [
         {
-            containerPort = 80,
-            hostPort = 80
-            protocol = "tcp"
+          containerPort = 80,
+          hostPort      = 80
+          protocol      = "tcp"
         }
-    ]
+      ]
     }
   ])
 }
@@ -50,8 +50,8 @@ resource "aws_ecs_service" "nginx" {
   cluster         = module.ecs.cluster_id
   task_definition = aws_ecs_task_definition.nginx_task.id
   desired_count   = 1
-  # iam_role        = aws_iam_role.foo.arn
-  # depends_on      = [aws_iam_role_policy.foo]
+  iam_role        = aws_iam_role.ecs_role.arn
+  depends_on      = [aws_iam_role_policy.ecs_policy]
 
   load_balancer {
     target_group_arn = aws_lb_target_group.nginx_http_tg.arn
@@ -61,7 +61,7 @@ resource "aws_ecs_service" "nginx" {
 
   network_configuration {
     assign_public_ip = true
-    security_groups = [ aws_security_group.nginx_http_sg ]
-    subnets = data.aws_subnet_ids.default.ids
+    security_groups  = [aws_security_group.nginx_http_sg]
+    subnets          = data.aws_subnet_ids.default.ids
   }
 }
---
+++ modules/nginx/iam-role.tf
@@ -0,0 +1,63 @@
+# IAM role policy for ECS
+resource "aws_iam_role_policy" "ecs_policy" {
+  name = "ecs_policy"
+  role = aws_iam_role.ecs_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:GetRegistryPolicy",
+          "ecr:DescribeImageScanFindings",
+          "ecr:GetLifecyclePolicyPreview",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:DescribeRegistry",
+          "ecr:DescribePullThroughCacheRules",
+          "ecr:DescribeImageReplicationStatus",
+          "ecr:GetAuthorizationToken",
+          "ecr:ListTagsForResource",
+          "ecr:ListImages",
+          "ecr:BatchGetRepositoryScanningConfiguration",
+          "ecr:GetRegistryScanningConfiguration",
+          "ecr:BatchGetImage",
+          "ecr:DescribeImages",
+          "ecr:DescribeRepositories",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetRepositoryPolicy",
+          "ecr:GetLifecyclePolicy",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+# IAM Role for ECS
+resource "aws_iam_role" "ecs_role" {
+  name = "ecs_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = aws_iam_policy.ecs_policy.arn
+}
